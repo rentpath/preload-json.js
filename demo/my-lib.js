a1ee6c0e405fe26204f8fff4806469a13db6fc1a
Caleb Wright <awright@rentpath.com>
2016-02-19 16:44:37 -0500

    feat: Init with very basic demo of concept

---
+++ demo/my-lib.js
@@ -0,0 +1,29 @@
+var request = new XMLHttpRequest()
+request.open('GET', 'https://api.github.com/users/c0/events', true)
+
+var data;
+
+request.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    data = JSON.parse(this.response)
+    console.log('AJAX data', data)
+  } else {
+    // We reached our target server, but it returned an error
+  }
+}
+
+request.onerror = function() {
+  console.log('loading error')
+  // There was a connection error of some sort
+}
+
+request.send()
+
+
+// PD
+
+
+
+//
+//
