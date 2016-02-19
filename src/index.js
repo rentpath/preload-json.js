811ba3165e0ae7a59e672e9c08600e276af8f6d9
Caleb Wright <awright@rentpath.com>
2016-02-19 16:58:35 -0500

    feat: Added basic commands

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+
+function notify (name, data) {
+  console.log('notify', name)
+}
+
+function subscribe (name, callback) {
+  console.log('subscribe', name)
+}
+
+export function push(command) {
+  console.log('push', command);
+  var action = command[0]
+  var argument = command.shift()
+
+  // white-list actions
+  switch (action) {
+    case 'notify':
+      notify.apply(this, arguments)
+      break;
+    case 'subscribe':
+      subscribe.apply(this, arguments)
+      break;
+  }
+}
+
+// Hookup
+if (typeof window === 'object' && typeof window.parallelData !== 'undefined') {
+  while (window.parallelData.length) {
+    push(window.parallelData.shift())
+  }
+  console.log('replace window.parallelData')
+  window.parallelData = { push: push }
+}
+
 export default {}
