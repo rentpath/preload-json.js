1bcb42bbcaabe540071a16b0b4bd2743a71d0964
Caleb Wright <awright@rentpath.com>
2016-02-22 16:53:49 -0500

    style: Fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
+const subscribers = {}
 
-var subscribers = {};
-
-function notify (name, data) {
+function notify(name, data) {
   console.log('notify', name)
-  let toNotify = subscribers[name] || [];
+  const toNotify = subscribers[name] || []
   for (let i = 0, len = toNotify.length; i < len; i++) {
     toNotify[i](data)
   }
 }
 
-function subscribe (name, callback) {
+function subscribe(name, callback) {
   console.log('subscribe', name)
   subscribers[name] = subscribers[name] || []
   subscribers[name].push(callback)
 }
 
 function push(command) {
-  console.log('cmd', command);
-  var action = command[0]
+  const action = command[0]
+  console.log('cmd', command)
   command.shift()
 
   // white-list actions
   switch (action) {
     case 'notify':
       notify.apply(this, command)
-      break;
+      break
     case 'subscribe':
       subscribe.apply(this, command)
-      break;
+      break
+    default:
+      // no-op
   }
 }
 
@@ -37,7 +38,7 @@ if (typeof window === 'object' && typeof window.parallelData !== 'undefined') {
     push(window.parallelData.shift())
   }
   console.log('replace window.parallelData')
-  window.parallelData = { push: push }
+  window.parallelData = { push }
 }
 
-export default { push: push }
+export default { push }
